736c936afb89a82ec4362e15ec227bf31006af9c
Coding Blood <pravinkumar2108@gmail.com>
2023-05-04 22:37:17 +0530

    Getting Started with Abstraction Encapsulation and Inheritance

--- Learning/Advanced OOP's Concept/Inheritance/Inheritance1.cpp
+++ Learning/Advanced OOP's Concept/Inheritance/Inheritance1.cpp
@@ -1,6 +1,7 @@
 /*One of the major advantages of Inheritance is code reusability */
 /*Access Specifier 
 Private: This cannot be access outside the class this can only be accessed by the member Functions declared in the public access specifier 
-Public:
+Public: This can be accesed anywhere in the class and outside the class
 Protected: This access specifier lies between Private and Public Access Specifier so this kind of access specifier may or may not be accessed according to our needs 
+like if we have declared some property protected then only the child class can access it and the parent class can have access to the property
 */
