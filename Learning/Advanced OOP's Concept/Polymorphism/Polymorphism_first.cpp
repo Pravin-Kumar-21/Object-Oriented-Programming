6203eaa4017fb15ca8bd2ef57bcbfd240e3ceb1c
Coding Blood <pravinkumar2108@gmail.com>
2023-05-10 23:44:16 +0530

    Function Overloading

---
+++ Learning/Advanced OOP's Concept/Polymorphism/Polymorphism_first.cpp
@@ -0,0 +1,24 @@
+/*Ploymorphism are of two types 
+1. Compile Time Polymorphism
+2. Runtime Polymorphism
+*/
+#include <iostream>
+using namespace std;
+
+
+int test(int a, int b) {        // using same function name but passing different number of parameters
+                                        // this type of Polymorphism is called Function Overloading...
+}
+
+int test(int a) {
+
+}
+
+int test() {
+
+}
+
+int main() {
+
+}
+
